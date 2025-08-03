3937f6b817e420284076377ee5b1b4973a8ae11a
Vinodbiradar09 <vinodjb07@gmail.com>
2025-08-03 11:24:34 +0530

    socket setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+import http from "http";
 import { app } from "./app.js";
 import { connectDB } from "./src/db/db.js";
+import { initializeSocket } from "./socket.js";
+
+
+const server = http.createServer(app);
 
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 4000, () => {
+        server.listen(process.env.PORT || 4000, () => {
             console.log(`server is running at local host ${process.env.PORT}`);
         })
     })
